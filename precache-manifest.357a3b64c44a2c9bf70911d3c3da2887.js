0f1b5abe31c183f7b45735b8a856331affecc113
Aniket Pradhan <aniket_pro@outlook.com>
2020-08-17 15:13:09 +0530

    Updates

---
+++ precache-manifest.357a3b64c44a2c9bf70911d3c3da2887.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5637930efcbe25bddcfb8db0b67513",
+    "url": "/color-shades-generator/index.html"
+  },
+  {
+    "revision": "ef9592fd707b7e691a8c",
+    "url": "/color-shades-generator/static/css/main.d4b9a2c0.chunk.css"
+  },
+  {
+    "revision": "5861fb55031013dd29eb",
+    "url": "/color-shades-generator/static/js/2.7cc8715e.chunk.js"
+  },
+  {
+    "revision": "62afbf7b6a84202ba7762582324f8a1a",
+    "url": "/color-shades-generator/static/js/2.7cc8715e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef9592fd707b7e691a8c",
+    "url": "/color-shades-generator/static/js/main.aac9f2a7.chunk.js"
+  },
+  {
+    "revision": "526e22ae599e38853df0",
+    "url": "/color-shades-generator/static/js/runtime-main.b66aa818.js"
+  },
+  {
+    "revision": "5d1a7c95a045bae3fa006285e8203b66",
+    "url": "/color-shades-generator/static/media/sound1.5d1a7c95.mp3"
+  }
+]);
